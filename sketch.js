c59ccc17e40cfb5086349a752809b2e6ca6acbaf
Akul9 <73068716+Akul9@users.noreply.github.com>
2021-06-16 20:03:06 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,62 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var database;
+var position;
+var form, player, game;
+ var charaters, challenger,pirate,rockstar,royalAgent;
+ var challengerImg, pirateImg, rockstarImg, royalAgentImg, backgroundImg, bulletImg, formBackgroundImg, bulletImg;
+function preload(){
+  challengerImg = loadImage("./Images/Challenger.png");
+ // challengerImg.scale = 0.1;
+  pirateImg = loadImage("./Images/Pirate.png");
+  rockstarImg = loadImage("./Images/Rockstar.png");
+  royalAgentImg = loadImage("./Images/royalAgent.png");
+  backgroundImg = loadImage("./Images/Background.png");
+  bulletImg = loadImage("./Images/Bullet.png");
+  formBackgroundImg = loadImage("./Images/FormBackground.png");
+  bulletImg = loadImage("./Images/Bullet.png");
+}
+
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+
+  var characterPosition = database.ref('character/position');
+  characterPosition.on("value", updateDistance);
+}
+
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+  if(gameState === 2){
+    game.end();
+  }
+}
+
+
+function updateDistance(data){
+  position=data.val();
+  charaters[index-1].x= position.x,
+  charaters[index-1].y= position.y
+}
+
+
+function setDistance(x,y){
+  database.ref("character/position").set({
+    'x': position.x + x,
+    'y': position.y + y
+  })
+}
